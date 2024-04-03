d63552535328715ca9ce4361d2b62f22f1c7baac
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-04-03 10:54:57 +0200

    complete html to show data Set up pagination

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -21,13 +21,31 @@ export default {
 </script>
 
 <template>
-  <h1>Home page</h1>
+  <h1 class="text-center mb-3">Sofa and Sofa!</h1>
   <div class="container">
     <div class="row">
-      <div v-for="product in products" class="col-3">
-        <h2>
+      <div v-for="product in products" class="col-4 mb-2">
+        <div class="card w-100" style="min-height: 478px">
+          <img :src="product.image" class="card-img-top" />
+          <div class="card-body">
+            <h5 class="card-title">{{ product.title }}</h5>
+            <p class="card-text">
+              {{ product.description.slice(0, 100) }}
+            </p>
+            <router-link :to="{ name: 'product' }">
+              <button class="btn btn-primary">See details</button>
+            </router-link>
+            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+          </div>
+        </div>
+        <!-- <h2>
           {{ product.title }}
         </h2>
+        <div>
+          <img :src="product.image" alt="" />
+        </div>
+        <p>{{ product.description.slice(0, 100) }} ...</p>
+        <p>{{ product.price }}€</p> -->
       </div>
     </div>
   </div>
