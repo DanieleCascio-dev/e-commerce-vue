0b85e21e315c76c558bfd98fedc9fa38afafb55f
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-04-03 10:29:09 +0200

    complete axios call Set up html to show data(wip)

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,11 +6,15 @@ export default {
   data() {
     return {
       store,
+      products: [],
     };
   },
   created() {
+    // axios call to get all products
     axios.get(`${this.store.baseUrl}/api/products`).then((resp) => {
-      console.log(resp);
+      // console.log(resp.data.results.data);
+      this.products = resp.data.results.data;
+      console.log(this.products);
     });
   },
 };
@@ -18,6 +22,15 @@ export default {
 
 <template>
   <h1>Home page</h1>
+  <div class="container">
+    <div class="row">
+      <div v-for="product in products" class="col-3">
+        <h2>
+          {{ product.title }}
+        </h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
